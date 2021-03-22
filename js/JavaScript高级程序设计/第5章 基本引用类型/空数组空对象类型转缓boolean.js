fdbd1a0dff64b4deba8e418a608649236d55485d
Ahuang0107 <Seto1920748235@126.com>
2021-03-22 08:00:07 +0800

    update

---
+++ js/JavaScript高级程序设计/第5章 基本引用类型/空数组空对象类型转缓boolean.js
@@ -0,0 +1,7 @@
+console.log(([])?true:false);
+console.log(([]==false?true:false));
+console.log((![]==false?true:false));
+console.log(({}==false)?true:false);
+console.log(Boolean([]))
+console.log(Boolean({}))
+console.log([].values().toString())
