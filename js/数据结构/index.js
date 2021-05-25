652de172a13e3eb81ac59a958280cea3645af215
Ahuang0107 <Seto1920748235@126.com>
2021-05-25 08:00:19 +0800

    update

--- js/数据结构/index.js
+++
@@ -1,5 +0,0 @@
-import * as _util from './util';
-
-export const util = _util;
-
-export { default as BinarySearchTree } from './树/BinarySearchTree';
--- js/数据结构/test.js
+++
@@ -1,8 +0,0 @@
-import {BinarySearchTree} from 'index';
-
-const tree=new BinarySearchTree();
-
-tree.insert(1);
-tree.insert(2);
-tree.insert(3);
-console.log(tree);
