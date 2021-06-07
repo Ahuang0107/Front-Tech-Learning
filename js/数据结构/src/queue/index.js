7f44490e782804e297995a148e4fa359410ccf46
Elase Huang <elase.huang@outlook.com>
2021-06-06 18:40:04 -0700

    A 队列实践

--- js/数据结构/src/queue/index.js
+++ js/数据结构/src/queue/index.js
@@ -1,4 +1,5 @@
 import Queue from "./Queue.js";
+import DeQue from "./DeQue.js";
 
 export function hotPotato(elementList, num) {
     const queue = new Queue()
@@ -16,4 +17,25 @@ export function hotPotato(elementList, num) {
         eliminated: eliminatedList,
         winner: queue.dequeue()
     }
+}
+
+export function palindromeChecker(string) {
+    if (string === undefined || string === null || string.length === 0) {
+        return false
+    }
+
+    const stringArray = string.split('')
+    const queue = new DeQue()
+    stringArray.forEach(char => {
+        queue.enqueue(char)
+    })
+    while (queue.size() > 1) {
+        if (queue.peekBack() === queue.peekFront()) {
+            queue.removeBack()
+            queue.removeFront()
+        } else {
+            return false
+        }
+    }
+    return true
 }
--- js/数据结构/src/queue/index.spec.js
+++ js/数据结构/src/queue/index.spec.js
@@ -1,11 +1,20 @@
 import 'mocha'
 import {expect} from "chai";
-import {hotPotato} from "./index.js";
+import {hotPotato, palindromeChecker} from "./index.js";
 
 describe('Queue Usage', () => {
     it('hotPotato', () => {
         const names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl']
         const result = hotPotato(names, 7)
         expect(result.winner).to.equal('John')
+    })
+    it('palindromeChecker', () => {
+        expect(palindromeChecker('a')).to.equal(true)
+        expect(palindromeChecker('madam')).to.equal(true)
+        expect(palindromeChecker('racecar')).to.equal(true)
+        expect(palindromeChecker('raccar')).to.equal(true)
+        expect(palindromeChecker('racecer')).to.equal(false)
+        expect(palindromeChecker('raccer')).to.equal(false)
+        expect(palindromeChecker('')).to.equal(true)
     })
 })
